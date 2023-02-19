6418c769839f04e66630cfbae2c98724050b4846
Vitaliy Prikota <mr.prikota@gmail.com>
2023-02-19 14:47:57 +0100

    Added tests for getting values

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -5,15 +5,32 @@
 
 constexpr const char* TEST_DB_PATH = "./test-db.bin";
 
-TEST_CASE("Test db size")
+namespace
 {
+    void CreateDb()
     {
         db::KeyMapped db(TEST_DB_PATH, true);
         db.Add("Key", "Value");
         db.Add("Key1", "Value1");
         db.Add("Key2", "Value2");
     }
+}
+
+TEST_CASE("Database size")
+{
+    CreateDb();
 
     db::KeyMapped db(TEST_DB_PATH);
     CHECK(db.Size() == 3);
+}
+
+TEST_CASE("Read and write")
+{
+    CreateDb();
+
+    db::KeyMapped db(TEST_DB_PATH);
+    CHECK(db.Get("Key") == "Value");
+    CHECK(db.Get("Key1") == "Value1");
+    CHECK(db.Get("Key2") == "Value2");
+    CHECK(db.Get("Key3") == "");
 }
